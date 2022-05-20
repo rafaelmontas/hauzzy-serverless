c9b33c87c05358a9f491de3d1d6feb238cbd882a
Rafael Montas <rafael.montas@hauzzy.com>
2022-05-20 17:46:16 -0400

    Change mocha version to fix error and add test

--- get-property/tests/unit/test-handler.js
+++ get-property/tests/unit/test-handler.js
@@ -6,11 +6,11 @@ const expect = chai.expect;
 let context;
 
 
-// const eventNull =  {
-//   pathParameters: {
-//     id: '1b720679-aa36-48cc-b066-0984c1586x00'
-//   }
-// }
+const eventNull =  {
+  pathParameters: {
+    id: '1b720679-aa36-48cc-b066-0984c1586x0000'
+  }
+}
 const event = {
   pathParameters: {
     id: '1b720679-aa36-48cc-b066-0984c1586b99'
@@ -19,12 +19,12 @@ const event = {
 
 describe('Tests index', function () {
   this.timeout(30000)
-  // it('verifies successful response when pathParameters are not valid', async () => {
-  //   const result = await app.lambdaHandler(eventNull, context)
+  it('verifies successful response when pathParameters are not valid', async () => {
+    const result = await app.lambdaHandler(eventNull, context)
 
-  //   expect(result).to.be.an('object');
-  //   expect(result.statusCode).to.equal(500);
-  // });
+    expect(result).to.be.an('object');
+    expect(result.statusCode).to.equal(500);
+  });
   it('verifies successful response when pathParameters are provided', async () => {
     const result = await app.lambdaHandler(event, context)
 
